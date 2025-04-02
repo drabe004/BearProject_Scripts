22188726a24a561f6ae16d7a38c8cdfb9160d9c5
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-02 16:08:54 -0500

    Update 1RENAME_fastas_py.sh
    cleanup paths

--- 1RENAME_fastas_py.sh
+++ 1RENAME_fastas_py.sh
@@ -3,11 +3,9 @@
 #SBATCH --ntasks=2
 #SBATCH --mem=100g
 #SBATCH --tmp=50g
-#SBATCH -p astyanax
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=drabe004@umn.edu
 
-cd /panfs/jay/groups/26/mcgaughs/drabe004/BEARS
+
+cd /panfs/to/your/dir
 
 modue load python
 
