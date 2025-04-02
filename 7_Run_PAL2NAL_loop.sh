bc6ee3c60316ff764b7bcb37175f55231a2e7378
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-02 17:08:30 -0500

    Update and rename 7Run_PAL2NAL_loop.sh to 7_Run_PAL2NAL_loop.sh

--- 7_Run_PAL2NAL_loop.sh
+++ 7_Run_PAL2NAL_loop.sh
@@ -3,11 +3,14 @@
 #SBATCH --ntasks=20
 #SBATCH --mem=400g
 #SBATCH --tmp=200g
-#SBATCH -p astyanax
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=drabe004@umn.edu
+#SBATCH --mail-user=youremail
 
-cd /panfs/jay/groups/26/mcgaughs/drabe004/BEARS
+cd /path/to/your/files
+
+#### YOU'LL NEED TO DOWNLOAD THE PAL2NAL PEARL SCRIPT ./pal2nal.v14/pal2nal.pl AVAILABLE HERE: https://www.bork.embl.de/pal2nal/#Download
+## No dependancies, simple, elegant pearl script you just need to run it on a loop )
+#### Check your files for matching file name formats (e.g. ""$input_dir"/*prot*.fasta; do" and "find "$input_dir" -maxdepth 1 -name "${prefix}_refseq*.fasta" | grep -v 'prot' | head -n 1)") 
 
 # Define input and output directories
 input_dir="Renamed_output_ALL"
