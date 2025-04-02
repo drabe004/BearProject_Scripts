a8230d49f0e1a68d360958a884087704b5f3cf3d
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-02 16:16:44 -0500

    Update 2RENAME_alns.sh

--- 2RENAME_alns.sh
+++ 2RENAME_alns.sh
@@ -3,12 +3,10 @@
 #SBATCH --ntasks=2
 #SBATCH --mem=100g
 #SBATCH --tmp=50g
-#SBATCH -p astyanax
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=drabe004@umn.edu
 
-cd /panfs/jay/groups/26/mcgaughs/drabe004/BEARS
+
+cd /your/dir
 
 modue load python
 
-python3 RENAME_alns.py HillerGenes ###pathway to input files
+python3 script.py dir/to/input/files dir/to/output/files
