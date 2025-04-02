7cee1c629d8e3c86f042795167e841e0e2d1c8fe
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-02 17:21:52 -0500

    Update 8_WriteTrees.sh

--- 8_WriteTrees.sh
+++ 8_WriteTrees.sh
@@ -13,15 +13,22 @@ module load python3/3.7.1_anaconda
 
 module load conda
 
+#conda create -n newbioenv python=3.7 biopython
+#this is the venv for this script
+
 # Source the Conda initialization script
-source /common/software/install/migrated/anaconda/python3-2020.07-mamba/etc/profile.d/conda.sh
+#source /common/software/install/migrated/anaconda/python3-2020.07-mamba/etc/profile.d/conda.sh
+#This is a fix for a cluster module update issue 
+# export PYTHONPATH="${PYTHONPATH}:/path/to/new/environment/python/packages"
+
 
 # Activate the new Conda environment
 conda activate newbioenv
 
-# If needed, adjust PYTHONPATH for the new environment
-# Note: Adjusting PYTHONPATH is often not necessary when using Conda environments,
-# as dependencies should be managed within the environment itself.
-# export PYTHONPATH="${PYTHONPATH}:/path/to/new/environment/python/packages"
 
-python 8_writeTrees.py ALL_specieS_NBL.tre Hand_Edited_Alignments
+###INPUT
+#ALL_specieS_NBL.tre  a master species topology with no branch lengths 
+#Pal2nal_alns a directory of codon alignments from PAL2nal. 
+## suggest running clean up script after pal2nal for second pass
+
+python 8_writeTrees.py ALL_specieS_NBL.tre Pal2nal_alns
