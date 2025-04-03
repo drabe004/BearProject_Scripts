4bcfbbc182f608e4462f69db9f347c5037496a81
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-03 09:05:53 -0500

    Update and rename 12BTRelax.sh to 11_Relax.sh
    cleanup script

--- 11_Relax.sh
+++ 11_Relax.sh
@@ -3,32 +3,32 @@
 #SBATCH --ntasks=25
 #SBATCH --mem=250g
 #SBATCH --tmp=150g
-#SBATCH -p astyanax
 #SBATCH --mail-type=ALL  
-#SBATCH --mail-user=drabe004@umn.edu
+#SBATCH --mail-user=youremail
 
+####This script runs the HYPHY program RELAX on a set of alignments and tree files with matching prefixes "Gene1_blahblah.fasta" and "Gene1_blahblah.tre" and outputs results to a separarate directory 
 
 # Load necessary modules
 module use /common/software/modulefiles/manual/common
 module load hyphy/2.5.53
 
 # Directory containing alignment and tree files
-input_dir=/panfs/jay/groups/26/mcgaughs/drabe004/BEARS/Clean_Alignments/SUBSET
+input_dir=your/clean/codon/alignments
 
 # Output directory for results
-output_dir=/panfs/jay/groups/26/mcgaughs/drabe004/BEARS/Relax_results
+output_dir=your/Relax/results
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
 
 # Loop through alignment files
-for alignment_file in "$input_dir"/*_clean.fasta; do
+for alignment_file in "$input_dir"/_*.fasta; do
     # Extract prefix from alignment filename
-    alignment_prefix=$(basename "$alignment_file" _clean.fasta)
+    alignment_prefix=$(basename "$alignment_file"_*.fasta)
     
     # Construct corresponding tree filename
-    tree_file="$input_dir/${alignment_prefix}_clean_FGBG.tre"
+    tree_file="$input_dir/${alignment_prefix}_*.tre"
 
  # Construct output filename
     output_file="$output_dir/${alignment_prefix}_relax"
