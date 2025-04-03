f4369ed24aa79ecfcb2f9f44ea6cb926ead73c18
drabe004 <63505920+drabe004@users.noreply.github.com>
2025-04-03 09:20:16 -0500

    Update 14_Extract_unrounded_pvalues.sh
    add annotations

--- 14_Extract_unrounded_pvalues.sh
+++ 14_Extract_unrounded_pvalues.sh
@@ -7,6 +7,9 @@
 #SBATCH --mail-user=youremail
 
 
+####This script recursively searches a given directory for JSON files ending with BUSTED.json, extracts p-values from the "test results" section, and writes the extracted gene prefixes and p-values to a CSV file. Note that this is the only place you can find unrounded p-values for proper multiple test correction
+
+
 path/to/your/dir
 
 module load python
